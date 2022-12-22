465403ea407592c37e2ab5e58414993f9c6d29ba
Guillaume Robert <guillaumerober@gmail.com>
2022-12-22 13:32:40 +0100

    header + headline wip
    Co-authored-by: Lopez <pOwn3d@users.noreply.github.com>

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import LogoKasa from '../../assets/logo.png'
+import imgHeadline from '../../assets/IMG.png'
+import './home.css'
 
 export default function Home() {
     return (
-        <div>
-            <h1>Kasa</h1>
-        </div>
+        <>
+            <header>
+                <img src={LogoKasa} alt="logo Kasa"/> 
+            </header>
+            <section class="headline">
+                <div class="headline_img_container">
+                    <img src={imgHeadline} alt="décor rocheux"/>
+                    <h1>Chez vous, partout et ailleurs</h1>
+                </div>
+            </section>
+        </>
     )
 }
